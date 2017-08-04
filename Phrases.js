35ad639cd7cddcef1f87a8e5496c94afd33ed028
SJ Simon Jay (5054) <e0030417@agna.amgreetings.com>
2017-08-03 20:35:48 -0400

    Apositives fixed, adjectives suck!

--- Phrases.js
+++ Phrases.js
@@ -1,13 +1,11 @@
 const Phrases = [
 "Hope your day is filled with",
-"Hope your day is just like your",
-"Without you, life would just be",
+"Hope your day is just like",
 "Without you, I wouldn't have",
 "I love you as much as I love",
-"I was so excited about your birthday I forgot to bring you",
-"There's only one way to describe the feeling you give me,",
-"Thank you for the greatest gift of all,",
-"I can't imagine life without your",
+"I was so excited about your birthday I forgot to bring",
+"Thank you for the greatest gift of all",
+"I can't imagine life without",
 "You make me see the good in this world, like",
 "Life without you is like a world without",
 "For this special day I bought you",
@@ -18,4 +16,18 @@ const Phrases = [
 "You remind me to always be thankful for",
 "I wrote you a love song, I call it"
 "If you and I had a love song, it would be called"
-"To a special friend, who always brought me"];
+"To a special friend, who always brought me",
+"You've always brought the greatest gift of all to my life:",
+"You were the one who finally made me understand",
+"I always thought you were lame until you showed me",
+"This place sucked until you came along, well you and",
+"I've got 99 problem but thanks to you I no longer deal with",
+"You've shown me how much a person can love",
+"You've always been able to cheer me up, especially with",
+"Thank you so much for",
+"Simply...",
+"You showed me the true meaning of",
+"Shh... don't say anything, I understand everything now, even",
+"Without you, I would have no idea how to deal with",
+"We could be happy ever after... Just you, me, and",
+"You've always been my favorite I superhero, well... next to"];
